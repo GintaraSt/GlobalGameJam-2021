c119c4f0b71a99cb9b2017e19d249b61a14a77c9
Airidas <airidas.gabe@gmail.com>
2021-01-30 11:22:12 +0200

    New levels added

---
+++ Assets/Scripts/EnterScene.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EnterScene : MonoBehaviour
+{
+    public string SceneName;
+    public void EnterNextScene()
+    {
+        SceneManager.LoadScene(SceneName);
+    }
+}
